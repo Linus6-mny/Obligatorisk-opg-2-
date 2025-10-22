ae2f4ba87a449df87e64284eb65a20acfe560cef
Linus6-mny <fremstadlinus67@gmail.com>
2025-10-22 14:24:43 +0200

    0987

--- registrer-klasse.php
+++ registrer-klasse.php
@@ -19,9 +19,9 @@
 <?php 
   if (isset($_POST ["registrerKlasseKnapp"]))
     {
-      $klassekode=$_klassekode ["klassekode"];
-      $klassenavn=$_klassenavn ["klassenavn"];
-      $studiumkode=$_studiumkode ["studiumkode"];
+      $klassekode= $_POST["klassekode"]
+      $klassenavn= $_POST ["klassenavn"];
+      $studiumkode= $_POST ["studiumkode"];
 
       if (!$klassekode || !$klassenavn|| !$studiumkode)
         {
