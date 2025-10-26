c6bc942a40225cb3b4ebcbc4bb8d7f2ce72689bc
Linus6-mny <fremstadlinus67@gmail.com>
2025-10-26 17:26:24 +0100

    00

---
+++ slett-student.php
@@ -0,0 +1,48 @@
+<?php  /* slett-student */
+/*
+/*  Programmet lager et skjema for å velge et student som skal slettes  
+/*  Programmet sletter det valgte student
+*/
+?> 
+
+<script src="funksjoner.js"> </script>
+
+<h3>Slett student</h3>
+
+<form method="post" action="" id="slettstudentSkjema" name="slettstudentSkjema" onSubmit="return bekreft()">
+  Klasse <input type="text" id="student" name="student" required /> <br/>
+  <input type="submit" value="Slett student" name="slettstudentKnapp" id="slettstudentKnapp" /> 
+</form>
+
+<?php
+  if (isset($_POST ["slettstudentKnapp"]))
+    {	
+      $student=$_POST ["student"];
+	  
+	  if (!$student)
+        {
+          print ("student m&aring; fylles ut");
+        }
+      else
+        {
+          include("db-tilkobling.php");  /* tilkobling til database-serveren utført og valg av database foretatt */
+
+          $sqlSetning="SELECT * FROM student WHERE student='$student';";
+          $sqlResultat=mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; hente data fra databasen");
+          $antallRader=mysqli_num_rows($sqlResultat); 
+
+          if ($antallRader==0)  /* student er ikke registrert */
+            {
+              print ("student finnes ikke");
+            }
+          else
+            {	  
+              $sqlSetning="DELETE FROM student WHERE student='$student';";
+              mysqli_query($db,$sqlSetning) or die ("ikke mulig &aring; slette data i databasen");
+                /* SQL-setning sendt til database-serveren */
+		
+              print ("F&oslash;lgende student er n&aring; slettet: $student  <br />");
+            }
+        }
+    }
+?> 
